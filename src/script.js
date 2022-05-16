f9ee8bb67803c91defb3395845bbcd28d91829f3
JohnMichaelDeveloper <johnfrontend@gmail.com>
2022-05-16 01:45:29 -0300

    Correção e Ajuste do código HDs/SSDs

--- src/script.js
+++ src/script.js
@@ -1,16 +1,3 @@
-// const url = "ddr4Kingston.html"
-// const ddr4K = document.querySelector('#ddr4K')
-
-// function openInNewTab(url){
-//   const win = window.open(url, '_blank')
-//   win.focus()
-// }
-// btn.addEventListener('click', () =>{
-//   openInNewTab(url)
-// })
- 
-
-
 
  
 // 
@@ -18,6 +5,12 @@ var ddr3K = {tipo: 'DDR3', fabricante: 'Kingston', capacidade: '4 Gb', preco: '1
 var ddr3S = {tipo: 'DDR3', fabricante: 'Samsung', capacidade: '8 Gb', preco: '240,00'};
 var ddr4K = {tipo: 'DDR4', fabricante: 'Kingston Fury', capacidade: '8 Gb', preco: '237,80'};
 var ddr4C = {tipo: 'DDR4', fabricante: 'Corsair Vengeance', capacidade: '8 Gb', preco: '245,00'};
+
+var tipo =[];
+var fabricante =[];
+var capacidade =[];
+var tipo =[];
+
 var opcao;
 var i, text, fLen;
  fLen = ddr3K.length;
@@ -26,7 +19,7 @@ var i, text, fLen;
 
 
 if(document.getElementById('ddr3K') === ddr3K){
-  opcao = ddr3K
+  opcao = ddr3K.tipo
 } else if(document.getElementById('ddr3S') === ddr3S){
   opcao = ddr3S
 } else if(document.getElementById('ddr4K') === ddr4K){
@@ -34,6 +27,7 @@ if(document.getElementById('ddr3K') === ddr3K){
 } else if(document.getElementById('ddr4C') === ddr4C){
   opcao = ddr4C
 }
+
 // se a opção for igual  ddr3K então
 switch(opcao){
    case ddr3K :
@@ -56,11 +50,7 @@ switch(opcao){
         text += "<div> Tipo " + ddr4C.tipo + "</div>";
         
       }
-
-
  }
-
-
 
 function Mudarestado(el) {
   var display = document.getElementById(el).style.display;
@@ -73,27 +63,63 @@ function Mudarestado(el) {
 }
 
 
-// function abrirProduto(){
-//   var prodMemorias = window.open("ddr4Kingston.html","secao","width:100,height=100,top=50,left=50");
-// }
+// ----------------- HDs/SSDs -------------------
+
+var descricao = ["Western Digital","Seagate","Samdisk","Kingston","Kingston","Western Digital"];
+var imagem = ["/img/HD_WD_500gb_70.jpg","/img/HD_Seagate_1Tb_70.jpg","/img/SSD_Samdisk_480Gb_70.jpg","/img/SSD_Kingston_960Gb_70.jpg","/img/SSD_Kingston_M2_5000Gb_70.jpg","/img/SSD_WD_M2_240Gb_70.jpg"];
+var interface = ["Interface: HD Sata III", "Interface: HD Sata III","Interface: SSD Sata III","Interface: SSD Sata III","Interface: SSD M2","Interface: SSD M2"];
+var capacidade = ["Capacidade: 500 Gb", "Capacidade: 1 Tb", "Capacidade: 480 Gb", "Capacidade: 960 Gb", "Capacidade: 500 Gb", "Capacidade: 240 Gb"];
+var preco = ["Preço: R$ ","Preço: R$ ","Preço: R$ ","Preço: R$ ","Preço: R$ ","Preço: R$ "];
+var custo = ["170,00","350,00","380,00","700,00","410,00","270,00"];    
 
 
-// var jan;
-
-// function abre_bas() { // abre a pagina de bolas de basquete
-
-//     jan = window.open("Bolas_bas.html", "jan", "toolbar=no,scrollbars=no,resizable=no,top=500,width=300,height=400");
-// }
-
-// var descricao = new Array()
-//    descricao[0] = "Bola de Basquete Wilson";
-//    descricao[1] = "Bola de Volei Wilson";
-//    descricao[2] = "Bola de Volei Penalty";
-
-
-//    function bas(){
-
-//     document.getElementById("des").innerHTML = descricao[0];
-//     document.getElementById("img").innerHTML = imagem[0];
-//     document.getElementById("pre").innerHTML = preco[0];
-//     }
+  function westernDigital(){       
+    document.getElementById("titDes").innerHTML = descricao[0];
+    document.getElementById("imgDes").innerHTML = "<img src=" + imagem[0] + " class=" + "imgPrd" + "></img>";    
+    document.getElementById("int").innerHTML = interface[0];
+    document.getElementById("cap").innerHTML = capacidade[0];
+    document.getElementById("pre").innerHTML =  "Preço: R$ " + "<span>" + custo[0] + "</span>";
+        
+  }
+  function seagate(){       
+    document.getElementById("titDes").innerHTML = descricao[1];
+    document.getElementById("imgDes").innerHTML = "<img src=" + imagem[1] + " class=" + "imgPrd" + "></img>";  
+    document.getElementById("int").innerHTML = interface[1];
+    document.getElementById("cap").innerHTML = capacidade[1];
+    document.getElementById("pre").innerHTML =  "Preço: R$ " + "<span>" + custo[1] + "</span>";
+          
+  }
+  function samdisk(){       
+    document.getElementById("titDes").innerHTML = descricao[2];
+    document.getElementById("imgDes").innerHTML = "<img src=" + imagem[2] + " class=" + "imgPrd" + "></img>";  
+    document.getElementById("int").innerHTML = interface[2];
+    document.getElementById("cap").innerHTML = capacidade[2];
+    document.getElementById("pre").innerHTML =  "Preço: R$ " + "<span>" + custo[2] + "</span>";
+         
+  }
+  function kingston960(){
+    document.getElementById("titDes").innerHTML = descricao[3];
+    document.getElementById("imgDes").innerHTML = "<img src=" + imagem[3] + " class=" + "imgPrd" + "></img>";  
+    document.getElementById("int").innerHTML = interface[3];
+    document.getElementById("cap").innerHTML = capacidade[3];
+    document.getElementById("pre").innerHTML =  "Preço: R$ " + "<span>" + custo[3] + "</span>";
+     
+  }
+  function kingston500(){
+    document.getElementById("titDes").innerHTML = descricao[4];
+    document.getElementById("imgDes").innerHTML = "<img src=" + imagem[4] + " class=" + "imgPrd" + "></img>";  
+    document.getElementById("int").innerHTML = interface[4];
+    document.getElementById("cap").innerHTML = capacidade[4];
+    document.getElementById("pre").innerHTML =  "Preço: R$ " + "<span>" + custo[4] + "</span>";
+    
+  }
+  function wD240(){
+    document.getElementById("titDes").innerHTML = descricao[5];
+    document.getElementById("imgDes").innerHTML = "<img src=" + imagem[5] + " class=" + "imgPrd" + "></img>";  
+    document.getElementById("int").innerHTML = interface[5];
+    document.getElementById("cap").innerHTML = capacidade[5];
+    document.getElementById("pre").innerHTML =  "Preço: R$ " + "<span>" + custo[5] + "</span>";
+     
+  }
+ 
+  
